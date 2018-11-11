749d6b5eb055c14395e0583b25ff5f0376ec398f
dpedratscher <doris.pedratscher@gmail.com>
2018-11-11 11:45:05 +0100

    catroid 50: first tests finished, now rebasing

---
+++ catroid/src/androidTest/java/org/catrobat/catroid/uiespresso/ui/fragment/SpriteListFragmentNewObjectFromSelection.java
@@ -0,0 +1,103 @@
+/*
+ * Catroid: An on-device visual programming system for Android devices
+ * Copyright (C) 2010-2018 The Catrobat Team
+ * (<http://developer.catrobat.org/credits>)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * An additional term exception under section 7 of the GNU Affero
+ * General Public License, version 3, is available at
+ * http://developer.catrobat.org/license_additional_term
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.catrobat.catroid.uiespresso.ui.fragment;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import junit.framework.Assert;
+
+import org.catrobat.catroid.ProjectManager;
+import org.catrobat.catroid.content.Project;
+import org.catrobat.catroid.ui.ProjectActivity;
+import org.catrobat.catroid.uiespresso.testsuites.Cat;
+import org.catrobat.catroid.uiespresso.testsuites.Level;
+import org.catrobat.catroid.uiespresso.util.rules.BaseActivityInstrumentationRule;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+import java.io.File;
+import java.io.IOException;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class SpriteListFragmentNewObjectFromSelection {
+	@Rule
+	public BaseActivityInstrumentationRule<ProjectActivity> baseActivityTestRule = new
+			BaseActivityInstrumentationRule<>(ProjectActivity.class, ProjectActivity.EXTRA_FRAGMENT_POSITION, ProjectActivity.FRAGMENT_SPRITES);
+
+	@Before
+	public void setUp() throws Exception {
+		createNoObjectsProject();
+		baseActivityTestRule.launchActivity();
+	}
+
+	@Category({Cat.AppUi.class, Level.Detailed.class})
+	@Test
+	public void testAddSpriteDefaultNameOfFile() {
+		File testfile = null;
+		String filename = null;
+
+		try {
+			testfile = File.createTempFile("Test", ".png", null);
+			filename = filenameWithoutExtension(testfile.getName());
+		}catch (IOException e) {
+			Assert.assertNull(e);
+		}
+
+		final Uri pathToFile = Uri.fromFile(testfile);
+
+		InstrumentationRegistry.getInstrumentation().runOnMainSync(new Runnable() {
+			@Override
+			public void run() {
+				baseActivityTestRule.getActivity()
+						.onActivityResult(ProjectActivity.SPRITE_FILE, Activity.RESULT_OK,
+								new Intent(Intent.ACTION_VIEW, pathToFile));
+			}
+		});
+
+		onView(withText(filename))
+				.check(matches(isDisplayed()));
+	}
+
+	private void createNoObjectsProject() {
+		Project project = new Project(InstrumentationRegistry.getTargetContext(), "SpriteListFragmentNewObjectFromSelection");
+		ProjectManager.getInstance().setProject(project);
+		ProjectManager.getInstance().setCurrentlyEditedScene(project.getDefaultScene());
+	}
+
+	private String filenameWithoutExtension(String filename) {
+		return filename.substring(0, filename.indexOf("."));
+	}
+}
